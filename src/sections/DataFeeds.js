c95610f5b6c24c2cfe6bf2a4fc3f0c8a1296c766
unknown <anas.452111@gmail.com>
2023-05-27 14:44:05 +0500

    UI Fixed

--- src/sections/DataFeeds.js
+++ src/sections/DataFeeds.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DataFeeds = () => {
   return (
     <>
-        <img src="/assets/images/dataFeeds.svg" className="w-full h-auto hidden sm:flex" alt=''/>
+        <img src="/assets/images/datafeeds.svg" className="w-full h-auto hidden sm:flex" alt=''/>
         <img src="/assets/images/datafeeds-sm.svg" className="w-full h-auto flex sm:hidden my-16" alt=''/>
     </>
     
