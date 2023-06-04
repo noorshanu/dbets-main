e829e7b655a8218d54c2283339171dbb517f0e6f
unknown <anas.452111@gmail.com>
2023-06-04 15:49:09 +0500

    Bg overlay added

--- src/sections/DataFeeds.js
+++ src/sections/DataFeeds.js
@@ -2,15 +2,26 @@ import React from "react";
 
 const DataFeeds = () => {
   return (
-    <div id="datafeeds" className='bg-[url("/public/assets/images/datafeed-bg.svg")] bg-cover my-12 sm:my-32'>
+    <div id="datafeeds" className='relative datafeed-bg bg-cover my-12 sm:my-32'>
+      <div className="bg-black bg-opacity-30 absolute inset-0"/>
       <img
         src="/assets/images/datafeed-lg.svg"
-        className="w-full h-auto hidden sm:flex my-10"
+        className="w-full h-auto invisible hidden sm:flex my-10"
+        alt=""
+      />
+      <img
+        src="/assets/images/datafeed-lg.svg"
+        className="w-full h-auto absolute inset-0 hidden sm:flex my-10"
         alt=""
       />
       <img
         src="/assets/images/datafeeds-sm.svg"
-        className="w-full h-auto flex sm:hidden"
+        className="w-full h-auto absolute inset-0 flex sm:hidden"
+        alt=""
+      />
+      <img
+        src="/assets/images/datafeeds-sm.svg"
+        className="w-full h-auto invisible flex sm:hidden"
         alt=""
       />
     </div>
