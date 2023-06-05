f2a8449855effe6df4d22f00316ca9de1ac1a495
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-05 14:40:33 +0530

    Update OfferingCard.js

--- src/components/OfferingCard.js
+++ src/components/OfferingCard.js
@@ -6,22 +6,22 @@ const OfferingCard = ({title, text, img}) => {
       <img
         src="/assets/images/box.svg"
         className="invisible flex group-hover:hidden h-full w-max"
-        loading="eager"
+        loading="eager" alt=""
       />
       <img
         src="/assets/images/platformBox-hover.svg"
         className="invisible  group-hover:flex hidden h-full w-max"
-        loading="eager"
+        loading="eager" alt=""
       />
       <div className="absolute inset-0 rounded-3xl  bg-opacity-50  bg-cover px-10 py-[1rem] flex flex-col justify-center space-y-8">
-        <img className="w-20 h-auto" src={img}  />
+        <img className="w-20 h-auto" src={img}   alt=""/>
         <h1 className="text-2xl font-medium font-orbitron text-white">
           {title}
         </h1>
         <p className="text-white font-pop text-[2vw] sm:text-[1vw] 2xl:text-sm max-w-[90%] sm:max-w-[70%] opacity-90">
           {text}
         </p>
-        <button className="bg-white group-hover:bg-[#54FF7F] w-28 py-2 rounded-md text-sm font-pop font-semibold">
+        <button className="bg-white group-hover:bg-[#54FF7F] w-28 py-2 rounded-md text-xs font-pop font-semibold">
           Learn more
         </button>
       </div>
